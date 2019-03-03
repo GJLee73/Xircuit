e3be52a86139d72f09833ad62ea7d44ab01815d3
gabriel <tla4256@gmail.com>
2019-03-03 16:22:19 +0900

    added button for manipulating block numbers
    yellow button in stage 1 added 1 color block randomly

---
+++ Assets/RewardButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RewardButton : MonoBehaviour {
+
+	public NeoBlock motherBlock;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		int randomColor = Random.Range (0, 8);
+		AddBlock (randomColor);
+	}
+
+	void AddBlock(int color, int amount = 1){
+		if (color > 7 || color < 0) {
+			throw new UnityException ("unvalid color number");
+		}
+		int[] args = new int[] {color, amount};
+		motherBlock.SendMessage ("AddBlock", color);
+	}
+
+	private class Roulette: MonoBehaviour {
+		public GameObject effectCircle;
+
+	}
+}
